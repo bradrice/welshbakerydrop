fe230d57758fa7e14367e87da241abf8fd058dbd
Brad Rice <bradrice1@gmail.com>
2015-08-26 16:48:37 -0400

    Still not pushsing data in

--- app/admin/admin.js
+++ app/admin/admin.js
@@ -11,22 +11,29 @@ var admin = angular.module('myApp.admin', ['ngRoute'])
 
 admin.controller('AdminCtrl', ["$scope", "$firebaseArray", "$firebaseObject", function( $scope, $firebaseArray, $firebaseObject) {
     var ref = new Firebase("https://welshbaker.firebaseio.com");
-    $scope.data = $firebaseObject(ref);
+    //$scope.data = $firebaseObject(ref);
+    var prodName = ref.child('Products/Product/ProductName');
+    $scope.data = $firebaseObject(prodName);
     //$scope.data = $firebaseObject(ref.child('products').child('productName'));
 // The obj variable will appear to be empty here and won't contain any remote data,
 // because the request to the server has not returned when we reach this line.
-    console.log($scope.data);
+    console.log($firebaseObject(prodName));
+    $scope.product = {};
 
-    $scope.products = $firebaseArray(ref);
-    $scope.products.$add({"products":{
-        "Id": "1002",
-        "Name": "6 Packs",
-        "Quantity": "6",
-        "Price": "28.88",
-        "ProductUrl": "http://www.welshbaker.com/boxdeals",
-        "Extra": "0",
-        "ExtraQuantity": "0",
-        "YourOption": "textarea976",
-        "CatalogId": "112"
-    }});
+    var products = $firebaseArray(prodName);
+
+    $scope.addProduct = function() {
+        // $add on a synchronized array is like Array.push() except it saves to the database!
+        products.$add({
+            Id: $scope.product.id,
+            Name: $scope.product.name,
+            Quantity: $scope.product.quantity,
+            Price: $scope.product.price,
+            ProductUrl: $scope.product.url,
+            Extra: $scope.product.extra,
+            ExtraQuantity: $scope.product.extraQuantity,
+            CatalogId: $scope.product.catalogId
+        });
+    };
+
 }]);
